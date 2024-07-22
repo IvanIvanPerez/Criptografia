a01ddfd85bea1e1381626f15f486067aea41f3c4
Iván Pérez <ivanperezf1997@gmail.com>
2024-07-22 11:35:37 +0200

    Subida

--- ejercicio12.py
+++ ejercicio12.py
@@ -6,10 +6,10 @@ from Crypto.Random import get_random_bytes
 
 
 plaintext_bytes = bytes("He descubierto el error y no volveré a hacerlo mal","UTF-8")
-clave_bytes = bytes.fromhex('E2CFF885901B3449E9C448BA5B948A8C4EE322152B3F1ACFA0148FB3A426DB74 ')
+clave_bytes = bytes.fromhex('E2CFF885901B3449E9C448BA5B948A8C4EE322152B3F1ACFA0148FB3A426DB74')
 nonce = b64decode('9Yccn/f5nJJhAt2S')
 
-datos_asociados_bytes = bytes("id=1","UTF-8")
+datos_asociados_bytes = bytes("","UTF-8")
 cipher = AES.new(clave_bytes, AES.MODE_GCM,nonce=nonce)
 
 cipher.update(datos_asociados_bytes)
--- ejercicio14.py
+++ ejercicio14.py
@@ -16,6 +16,6 @@ salt = bytes.fromhex("e43bb4067cbcfab3bec54437b84bef4623e345682d89de9948fbb0afed
 #print("maestra:", master_secret.hex())
 master_secret = bytes.fromhex("A2CFF885901A5449E9C448BA5B948A8C4EE377152B3F1ACFA0148FB3A426DB72")
 
-key1 = HKDF(master_secret, 16, salt, SHA512, 1)
+key1 = HKDF(master_secret, 32, salt, SHA512, 1)
 
 print("Clave: ", key1.hex())
